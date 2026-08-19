b5487db04a130e5c9de497071ffe0315d66e18a8
CARLOS FONSECA <carlos.fonseca12@uptc.edu.co>
2026-08-19 17:40:41 -0500

    Exepciones

--- co/edu/uptc/structures/SimpleList.java
+++ co/edu/uptc/structures/SimpleList.java
@@ -71,7 +71,7 @@ public class SimpleList<E> implements Collection<E>, List<E> {
 	}
 
 	@Override
-	public boolean containsAll(Collection<?> c) {
+	public boolean containsAll(Collection<?> c) throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException{
 		for (Object object : c) {
 			if(!contains(object)){
 				return false;
