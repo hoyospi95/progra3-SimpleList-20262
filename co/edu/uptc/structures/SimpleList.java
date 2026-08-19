13c7540af78625704f723e236f295754356af943
AASVF <angelfan701@gmail.com>
2026-08-19 18:30:41 -0500

    cambios metodo listIterator(int index)

--- co/edu/uptc/structures/SimpleList.java
+++ co/edu/uptc/structures/SimpleList.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleList<E> implements Collection<E>, List<E> {
 	private Node<E> head;
@@ -15,14 +17,18 @@ public class SimpleList<E> implements Collection<E>, List<E> {
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		Node<E> current = head;
+		int count = 0;
+		while (current != null) {
+			count += 1;
+			current = current.getNext();
+		}
+		return count;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return head == null;
 	}
 
 	@Override
@@ -102,13 +108,28 @@ public class SimpleList<E> implements Collection<E>, List<E> {
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		head = null;
 	}
 
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
+		if (index < 0 || head == null) {
+			return null;
+		}
+
+		Node currentNode = head;
+		int currentIndex = 0;
+
+		while (currentNode != null) {
+			if (currentIndex == index) {
+				return currentNode.getValue();
+			}
+			else {
+				currentIndex ++;
+				currentNode = currentNode.getNext();
+			}
+		}
+		
 		return null;
 	}
 
@@ -120,8 +141,21 @@ public class SimpleList<E> implements Collection<E>, List<E> {
 
 	@Override
 	public void add(int index, E element) {
-		// TODO Auto-generated method stub
+		Node<E> newNode = new Node<>(element);
 
+		if (index == 0) {
+			newNode.setNext(head);
+			head = newNode;
+			return;
+		}
+		
+		Node<E> aux = head;
+		for (int i = 0; i < index - 1; i++) {
+			aux = aux.getNext();
+		}
+
+		newNode.setNext(aux.getNext());
+		aux.setNext(newNode);
 	}
 
 	@Override
@@ -132,8 +166,16 @@ public class SimpleList<E> implements Collection<E>, List<E> {
 
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		Node<E> aux = head;
+		int counter = 0;
+		while(aux != null){
+			if(aux.getValue().equals(o)){
+				return counter;
+			}
+			aux = aux.getNext();
+			counter++;
+		}
+		return -1;
 	}
 
 	@Override
